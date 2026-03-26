08b9f075a2acb4373bc166c8232a1572b83853fa
Phutthi-1st <firstrtx.3080@gmail.com>
2026-03-26 22:20:57 +0700

    Add files via upload

---
+++ screens/calculation_history.dart
@@ -0,0 +1,15 @@
+class CalculationHistory {
+  final String id;
+  final String title;
+  final double usageHours;
+  final String costPerMonth;
+  final String costPerYear;
+
+  CalculationHistory({
+    required this.id,
+    required this.title,
+    required this.usageHours,
+    required this.costPerMonth,
+    required this.costPerYear,
+  });
+}
