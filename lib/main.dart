2e9a8a17b7616497eae1f2f1ef67a8e6b3343e49
Phutthi-1st <firstrtx.3080@gmail.com>
2026-03-26 22:22:50 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const PikaWattApp());
@@ -22,6 +23,7 @@ class PikaWattApp extends StatelessWidget {
         '/': (context) => const SplashScreen(),
         '/login': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const HomeDashboardScreen(),
       },
     );
   }
