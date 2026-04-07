b15ac8157036094ccfb13137e5ba43f003e340c8
Phutthi-1st <firstrtx.3080@gmail.com>
2026-04-07 23:04:04 +0700

    Create appliance_model.dart

---
+++ screens/models/appliance_model.dart
@@ -0,0 +1,15 @@
+class ApplianceBrandModel {
+  final String brand;
+  final String modelName;
+  final int watts;
+  final String imagePath;
+  final String category; 
+
+  ApplianceBrandModel({
+    required this.brand,
+    required this.modelName,
+    required this.watts,
+    required this.imagePath,
+    required this.category,
+  });
+}
